fc5405ce12145cf53949eb5956195584a7c48efe
Grégory Limoratto <gregorylimoratto@gmail.com>
2015-01-05 15:56:56 +0100

    Ajout d'un test permettant le débug du plugin

---
+++ Javascript.Generator.SpecflowPlugin.UnitTest/GeneratorTester.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TechTalk.SpecFlow.Generator;
+using TechTalk.SpecFlow.Generator.Interfaces;
+
+namespace Javascript.Generator.SpecflowPlugin.UnitTest
+{
+    [TestClass]
+    public class GeneratorTester
+    {
+        // Permet de débugger le plugin specflow
+        [TestMethod]
+        public void TestGenerator()
+        {
+            VsConfigReader configurationReader = new VsConfigReader();
+
+            var configurationHolder = configurationReader.ReadConfiguration();
+
+            var projectSettings = new ProjectSettings
+            {
+                ProjectName = "Javascript.Generator.SpecflowPlugin.UnitTest",
+                AssemblyName = "Javascript.Generator.SpecflowPlugin.UnitTest",
+                ProjectFolder = "",
+                DefaultNamespace = "Javascript.Generator.SpecflowPlugin.UnitTest",
+                ProjectPlatformSettings = new ProjectPlatformSettings(),
+                ConfigurationHolder = configurationHolder
+            };
+                var container = GeneratorContainerBuilder.CreateContainer(projectSettings.ConfigurationHolder, projectSettings);
+                var test = container.Resolve<ITestGenerator>();
+
+            var result = test.GenerateTestFile(new FeatureFileInput("SpecFlowFeature1.feature"), new GenerationSettings()
+            {
+                WriteResultToFile = false
+            });
+
+            // Résultat de l'exécution dans bin / debug / *.generatedspec
+            Assert.IsTrue(result.Success);
+        }
+    }
+}
