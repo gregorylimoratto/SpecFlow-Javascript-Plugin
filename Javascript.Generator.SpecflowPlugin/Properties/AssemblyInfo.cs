c791d27d64ebba554ded277bef9fb787a167ffe4
Grégory Limoratto <gregorylimoratto@gmail.com>
2015-03-24 15:20:24 +0100

    remise à 0 de la version

--- Javascript.Generator.SpecflowPlugin/Properties/AssemblyInfo.cs
+++ Javascript.Generator.SpecflowPlugin/Properties/AssemblyInfo.cs
@@ -36,4 +36,4 @@ using System.Runtime.InteropServices;
 // Vous pouvez spécifier toutes les valeurs ou indiquer les numéros de build et de révision par défaut 
 // en utilisant '*', comme indiqué ci-dessous :
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyInformationalVersion("1.0.0.4")]
+[assembly: AssemblyInformationalVersion("1.0.0.0")]
